c406a9f7b18b0e13f431e4018343a499dc0fdab6
OlgaR007 <141062731+OlgaR007@users.noreply.github.com>
2023-08-04 15:13:30 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 function dispalyTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.main.pressure;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
 }
 let apiKey = "c73d0606c6de7b8ca937dc74e3a4c75f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
