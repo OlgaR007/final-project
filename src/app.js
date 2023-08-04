50ce2d6e0cbd37b4d620bd3779cfb331161eb358
OlgaR007 <141062731+OlgaR007@users.noreply.github.com>
2023-08-04 16:47:47 +0200

    Updated weather icons

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 function formatDate(timestamp) {
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -29,14 +30,20 @@ function dispalyTemperature(response) {
   let windElement = document.querySelector("#wind");
   let pressureElement = document.querySelector("#pressure");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   pressureElement.innerHTML = response.data.main.pressure;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
+
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "c73d0606c6de7b8ca937dc74e3a4c75f";
 let city = "Odesa";
